ce62977c02068ae1dc18ed5159bf48fe222fa820
Andrea De Martino <ademartino1992@gmail.com>
2020-05-23 12:12:46 +0200

    Test con Class app extends Componen 2t

--- src/App.js
+++ src/App.js
@@ -10,11 +10,11 @@ class App extends Component {
   state = {
     persons: [
       {
-        nome: 'Andrea',
+        nome: 'NOME STATICO 1',
         eta: 27
       },
       {
-        nome: 'Paola',
+        nome: 'NOME STATICO 2',
         eta: 25
       }
     ]
@@ -27,7 +27,7 @@ class App extends Component {
     this.setState({
       persons: [
         {
-          nome: 'Pippo',
+          nome: 'NOME DINAMICO 1',
           eta: 27
         },
       ]
